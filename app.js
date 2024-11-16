cc07d597717c19f5551f8a59954834fc71d536ac
AndresCochez <andres.cochez@hotmail.com>
2024-11-16 13:27:41 +0100

    Gebruikersroutering configureren

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const logger = require('./middleware/logger');
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
+const userRouter = require('./routes/user');
 
 app.use(express.json());
+app.use(logger);
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
+app.use('/users', userRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
