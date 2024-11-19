a03c8cc9935d1e4753bf5400eb220ed12d3863ed
AndresCochez <andres.cochez@hotmail.com>
2024-11-19 14:12:08 +0100

    Routering voor producten instellen

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 const userRouter = require('./routes/user');
+const productRouter = require('./routes/product');
 
 app.use(express.json());
 app.use(logger);
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 app.use('/users', userRouter);
+app.use('/products', productRouter);
+
+// Error handling
+app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
