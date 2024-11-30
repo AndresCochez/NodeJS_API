3239961ab37c91a07c2c38e671418daeff4919a7
AndresCochez <andres.cochez@hotmail.com>
2024-11-30 16:51:08 +0100

    Finale versie van het project

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
+const authenticate = require('./middleware/auth');
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 const userRouter = require('./routes/user');
@@ -9,6 +10,7 @@ const productRouter = require('./routes/product');
 
 app.use(express.json());
 app.use(logger);
+app.use(authenticate);
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 app.use('/users', userRouter);
